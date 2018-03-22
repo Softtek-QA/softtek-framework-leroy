8e127a1d4f7c3b6133360c685d96d61344b9e510
marcos.oliveira <marcos.oliveira@softtek.com>
2018-03-22 10:32:15 -0300

    Inclusão da classe de serviços do projeto

---
+++ src/br/leroy/testes/plaf/web/desktop/hybris/api/AUTAPI.java
@@ -0,0 +1,29 @@
+package br.leroy.testes.plaf.web.desktop.hybris.api;
+
+/**
+ * 
+ * Classe responsável pela definição de serviços diversos do projeto
+ * 
+ * @author Softtek - QA
+ *
+ */
+public class AUTAPI {
+	
+	/**
+	 * 
+	 * Configurações de inicialização do projeto
+	 * 
+	 */
+	public void configInit() {
+		
+	}
+	
+	/**
+	 * 
+	 * Construtor padrão da classe de serviço para aplicação : VA
+	 * 
+	 */
+	public AUTAPI() {
+		
+	}
+}
