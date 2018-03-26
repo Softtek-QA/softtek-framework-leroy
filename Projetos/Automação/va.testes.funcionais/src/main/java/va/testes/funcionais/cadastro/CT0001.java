c19b1626b19accd0553f9de6e63efddbfc99fb5b
marcos.oliveira <marcos.oliveira@softtek.com>
2018-03-26 07:22:00 -0300

    Inclusão de arquivo com a massa de dados - Padrão de escrita (chave valor)

--- Projetos/Automação/va.testes.funcionais/src/main/java/va/testes/funcionais/cadastro/CT0001.java
+++ Projetos/Automação/va.testes.funcionais/src/main/java/va/testes/funcionais/cadastro/CT0001.java
@@ -2,27 +2,33 @@ package va.testes.funcionais.cadastro;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import va.testes.funcionais.md.AUTDataLoader;
 
 public class CT0001 {
 	
 	
 	
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		
 		System.setProperty("webdriver.chrome.driver", "../va.testes.funcionais/src/main/resources/chromedriver.exe");
 		ChromeOptions chrOptions = new ChromeOptions();
+		
 		
 		//chrOptions.addArguments("headless");
 		//chrOptions.addArguments("window-size=1200x1024");
 		
 		//WebDriver driverDoc = new ChromeDriver(chrOptions);
-		WebDriver driverDoc = new ChromeDriver();
+		WebDriver driverDoc = new ChromeDriver();		
 		
+		System.out.println(AUTDataLoader.carregarParametros(""));
 		
 	}
 
