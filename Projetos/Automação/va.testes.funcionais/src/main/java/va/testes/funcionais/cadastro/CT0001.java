c6e642a0c2af5afedcc6a86032ce919aab9083f4
marcos.oliveira <marcos.oliveira@softtek.com>
2018-03-26 07:15:58 -0300

    Inclusão de função para carregamento da massa de dados -  Arquivo padrão

--- Projetos/Automação/va.testes.funcionais/src/main/java/va/testes/funcionais/cadastro/CT0001.java
+++ Projetos/Automação/va.testes.funcionais/src/main/java/va/testes/funcionais/cadastro/CT0001.java
@@ -5,17 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CT0001 {
-
+	
+	
+	
 	@Test
 	public void test() {
 		
+		System.setProperty("webdriver.chrome.driver", "../va.testes.funcionais/src/main/resources/chromedriver.exe");
+		ChromeOptions chrOptions = new ChromeOptions();
+		
+		//chrOptions.addArguments("headless");
+		//chrOptions.addArguments("window-size=1200x1024");
+		
+		//WebDriver driverDoc = new ChromeDriver(chrOptions);
 		WebDriver driverDoc = new ChromeDriver();
 		
-		org.junit.Assert.assertSame("valor 2", "valor 2");
-		
-		//fail("Not yet implemented");
 		
 	}
 
